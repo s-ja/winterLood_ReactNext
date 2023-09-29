ba43ab36469e9865dfe53bcdc69836f879333364
seungji <dkstmdwl0615@naver.com>
2023-09-29 12:16:53 +0900

    es module system practice

--- section1/src/math.js
+++ section1/src/math.js
@@ -1,12 +1,13 @@
-function add(a, b) {
+export function add(a, b) {
   return a + b;
 }
 
-function sub(a, b) {
+export function sub(a, b) {
   return a - b;
 }
 
-module.exports = {
-  add,
-  sub,
-};
+// export { add, sub };
+
+export default function multiply(a, b) {
+  return a * b;
+}

1804f7dc56806b3b3e7875e579ced21a0b088a8c
seungji <dkstmdwl0615@naver.com>
2023-09-30 18:24:35 +0900

    start react

--- section1/src/index.js
+++ section1/src/index.js
@@ -1,6 +1,12 @@
 import mul, { add, sub } from "./math.js";
 
-console.log(add(1, 2)); // 3
-console.log(sub(1, 2)); // -1
+import randomcolor from "randomcolor";
 
-console.log(mul(10, 20)); // 200
+// console.log(add(1, 2)); // 3
+// console.log(sub(1, 2)); // -1
+
+// console.log(mul(10, 20)); // 200
+
+const color = randomcolor();
+
+console.log(color);

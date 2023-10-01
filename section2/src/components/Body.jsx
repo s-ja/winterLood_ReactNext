7ce7e072b2a749a2edfb23afca2655e4b8267a0d
seungji <dkstmdwl0615@naver.com>
2023-10-01 14:02:16 +0900

    react project basic practice

--- section2/src/components/Body.jsx
+++ section2/src/components/Body.jsx
@@ -1,18 +1,9 @@
+import "./Body.css";
+
 export default function Body() {
-  const user = {
-    name: "asj",
-    isLogedin: true,
-  };
-
-  if (user.isLogedin) {
-    return <div>my page / log out</div>;
-  } else {
-    return <div>please log in</div>;
-  }
-
-  //   return (
-  //     <>
-  //       {user.isLogedin ? <div>hello {user.name}</div> : <div>please log in</div>}
-  //     </>
-  //   );
+  return (
+    <div className="body">
+      <h1>body</h1>
+    </div>
+  );
 }

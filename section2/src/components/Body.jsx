b2e79d9c41b2f79b7d7eead5ba303537527d2609
seungji <dkstmdwl0615@naver.com>
2023-10-01 13:46:21 +0900

    react project basic practice

--- section2/src/components/Body.jsx
+++ section2/src/components/Body.jsx
@@ -1,7 +1,27 @@
 export default function Body() {
+  const number = 101;
+  const string = "Hello";
+  const bool = true;
+  const obj = {
+    a: 1,
+  };
+  const func = () => {
+    return "func";
+  };
+
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <>
+      <h1>{string}</h1>
+      <h2>
+        {number % 2 === 0 ? (
+          <div>{number} is even</div>
+        ) : (
+          <div>{number} is odd</div>
+        )}
+      </h2>
+      <h2>{bool}</h2>
+      <h2>{obj.a}</h2>
+      <h2>{func()}</h2>
+    </>
   );
 }

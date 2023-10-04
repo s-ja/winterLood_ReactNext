5b1f86b0e5b454dd1afe1b4240e593d6a57c4b72
s-ja <dkstmdwl0615@naver.com>
2023-10-04 13:35:27 +0900

    react state basic practice

--- section2/src/components/Body.jsx
+++ section2/src/components/Body.jsx
@@ -1,44 +1,15 @@
 import "./Body.css";
 import { useState } from "react";
 
-function Lightbulb({ light }) {
-  return (
-    <>
-      {light === "ON" ? (
-        <div style={{ backgroundColor: "orange" }}>ON</div>
-      ) : (
-        <div style={{ backgroundColor: "darkorange" }}>OFF</div>
-      )}
-    </>
-  );
-}
-
-function StaticLightbulb() {
-  console.log("static");
-  return <div style={{ backgroundColor: "darkorange" }}>OFF</div>;
-}
-
 export default function Body() {
-  const [light, setLight] = useState("OFF");
-  console.log(light);
+  const [name, setName] = useState("");
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
   return (
     <div className="body">
-      <Lightbulb light={light} />
-      <StaticLightbulb />
-      <button
-        onClick={() => {
-          setLight("ON");
-        }}
-      >
-        turn on light
-      </button>
-      <button
-        onClick={() => {
-          setLight("OFF");
-        }}
-      >
-        turn off light
-      </button>
+      <input type="text" value={name} onChange={onChangeName} />
     </div>
   );
 }

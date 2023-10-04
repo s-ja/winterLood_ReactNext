a7f2f5527ca9a07992649fe0c39470416b7e774d
s-ja <dkstmdwl0615@naver.com>
2023-10-04 14:55:15 +0900

    input practice

--- section2/src/components/Body.jsx
+++ section2/src/components/Body.jsx
@@ -2,14 +2,68 @@ import "./Body.css";
 import { useState } from "react";
 
 export default function Body() {
-  const [name, setName] = useState("");
+  // const [name, setName] = useState("");
+  // const [gender, setGender] = useState("");
+  // const [bio, setBio] = useState("");
+  const [state, setState] = useState({
+    name: "",
+    gender: "",
+    bio: "",
+  });
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
+  // const onChangeName = (e) => {
+  //   setState({
+  //     ...state,
+  //     name: e.target.value,
+  //   });
+  // };
+
+  // const onChangeGender = (e) => {
+  //   setState({
+  //     ...state,
+  //     gender: e.target.value,
+  //   });
+  // };
+
+  // const onChangeBio = (e) => {
+  //   setState({
+  //     ...state,
+  //     bio: e.target.value,
+  //   });
+  // };
+
+  const onChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
   };
+
   return (
     <div className="body">
-      <input type="text" value={name} onChange={onChangeName} />
+      <div>
+        <input type="text" value={state.name} onChange={onChange} name="name" />
+        <div>current value : {state.name}</div>
+        <div>{state.name.length}</div>
+      </div>
+      <div>
+        <select name="gender" id="" value={state.gender} onChange={onChange}>
+          <option value="">not asignable</option>
+          <option value="male">male</option>
+          <option value="female">female</option>
+        </select>
+        <div>selected gender : {state.gender}</div>
+      </div>
+      <div>
+        <textarea
+          name="bio"
+          id=""
+          cols="30"
+          rows="10"
+          onChange={onChange}
+          value={state.bio}
+        ></textarea>
+      </div>
     </div>
   );
 }

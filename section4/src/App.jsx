9a47345166451c11dd50b7dbced2e5dd4a15c3d5
s-ja <dkstmdwl0615@naver.com>
2023-10-08 14:28:53 +0900

    ref basic practice

--- section4/src/App.jsx
+++ section4/src/App.jsx
@@ -1,18 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    console.log(`count : ${count}, text : ${text}`);
+  }, [count, text]);
 
   const onClickButton = (value) => {
     setCount(count + value);
   };
 
+  const onChangeText = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <div className="App">
       <h1>simple counter</h1>
+      <section>
+        <input type="text" value={text} onChange={onChangeText} />
+      </section>
       <section>
         <Viewer count={count} />
       </section>

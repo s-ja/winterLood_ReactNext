994ff23d3c51f19f6e62a83e787f92a4cebb2273
s-ja <dkstmdwl0615@naver.com>
2023-10-11 15:42:37 +0900

    mock data construct

--- section7/src/App.jsx
+++ section7/src/App.jsx
@@ -4,8 +4,29 @@ import Header from "./components/Header";
 import TodoEditor from "./components/TodoEditor";
 import TodoList from "./components/TodoList";
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "mock data1",
+    createDate: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "mock data2",
+    createDate: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: true,
+    content: "mock data3",
+    createDate: new Date().getTime(),
+  },
+];
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(mockData);
 
   return (
     <div className="App">

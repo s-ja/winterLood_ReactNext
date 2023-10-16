fbb1fbb04e3815f06694ea4f98ae875aea0efb23
s-ja <dkstmdwl0615@naver.com>
2023-10-16 21:44:33 +0900

    useReducer practice start

--- section7/src/main.jsx
+++ section7/src/main.jsx
@@ -2,11 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import A from "./components/exam/A.jsx";
-import B from "./components/exam/B.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  // <App />
-  // <A />
-  <B />
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);

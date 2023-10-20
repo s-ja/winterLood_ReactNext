46a7c8ec4ae3c9f7dab4cc9a72de4d9b5207c371
s-ja <dkstmdwl0615@naver.com>
2023-10-20 11:28:14 +0900

    dynamic routing practice

--- section11/src/App.jsx
+++ section11/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 import Country from "./pages/Country";
-import NotFound from "./pages/NotFount";
+import NotFound from "./pages/NotFound";
 
 function App() {
   const nav = useNavigate();
@@ -17,7 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
-        <Route path="/country" element={<Country />} />
+        <Route path="/country/:code" element={<Country />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <div>
--- section11/src/pages/Country.jsx
+++ section11/src/pages/Country.jsx
@@ -1,3 +1,9 @@
+import { useParams } from "react-router-dom";
+
 export default function Country() {
-  return <div>Country</div>;
+  const params = useParams();
+
+  console.log(params);
+
+  return <div>Country : {params.code}</div>;
 }

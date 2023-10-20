46783971f883871ee2ec884a843d924a24ac8b11
s-ja <dkstmdwl0615@naver.com>
2023-10-20 11:18:57 +0900

    navigate practice

--- section11/src/App.jsx
+++ section11/src/App.jsx
@@ -1,18 +1,33 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 import Country from "./pages/Country";
 import NotFound from "./pages/NotFount";
 
 function App() {
+  const nav = useNavigate();
+
+  const onClick = () => {
+    nav("/search");
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/search" element={<Search />} />
-      <Route path="/country" element={<Country />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/country" element={<Country />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/search"}>Search</Link>
+        <Link to={"/country"}>Country</Link>
+
+        <button onClick={onClick}>Search</button>
+      </div>
+    </>
   );
 }
 
